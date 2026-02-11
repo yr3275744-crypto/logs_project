4de8af730c5c452742824fd3d41e56f027493a68
Ytshak_Raiyner <yr3275744@gmail.com>
2026-02-11 15:50:13 +0200

    declarate serech_sensitive_port_lines

--- analyzer.py
+++ analyzer.py
@@ -1,3 +1,8 @@
-def serech_external_ip(log_list:list):
+def search_external_ip(log_list:list):
     external_ip = [list[1] for list in log_list if not list[1].startswith("192.168") and not list[1].startswith("10.")]
     return external_ip
+
+def search_sensitive_port_lines(log_list:list):
+    sensitive_port_lines = [line for line in log_list if line[3] == "22" or line[3] == "23" or line[3] == "3389"]
+    return sensitive_port_lines
+
