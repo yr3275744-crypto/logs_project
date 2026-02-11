d193078ed8f461e51d1a06e4d78f515b234625c2
Ytshak_Raiyner <yr3275744@gmail.com>
2026-02-11 15:55:36 +0200

    declarate serech_big_size_lines

--- analyzer.py
+++ analyzer.py
@@ -6,3 +6,6 @@ def search_sensitive_port_lines(log_list:list):
     sensitive_port_lines = [line for line in log_list if line[3] == "22" or line[3] == "23" or line[3] == "3389"]
     return sensitive_port_lines
 
+def search_big_size_lines(log_list:list):
+    big_size_lines = [line for line in log_list if int(line[5]) > 5000]
+    return big_size_lines
