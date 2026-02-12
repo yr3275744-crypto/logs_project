92ab39dedf36496375cb50b8d7c3e8870e4b2da3
Ytshak_Raiyner <yr3275744@gmail.com>
2026-02-12 17:51:00 +0200

    add 1-4 parts

--- analyzer.py
+++ analyzer.py
@@ -1,3 +1,6 @@
+from reader import read_log_file
+log_list = read_log_file(r"C:\Users\יצחק ריינר\my_python\projects\logs_project\network_traffic.log")
+
 def search_external_ip(log_list:list):
     external_ip = [list[1] for list in log_list if not list[1].startswith("192.168") and not list[1].startswith("10.")]
     return external_ip
@@ -26,7 +29,8 @@ def counting_num_requests(log_list:list):
 def protocol_port_aranging(log_list:list):
     protocol_port_arrangement = {line[3]:line[4] for line in log_list}
     return protocol_port_arrangement
-
+#בחרתי לכתוב את הפונקציה להלן כך, כדי למעט הרבה סיבוכיות של חיפוש ברשימות שיוצאות מהפונקציות הקודמות. 
+#בדיעבד הבנתי שעדיף היה לעשות בשורה אחת, ובנוסף הקוד היה דינמי יותר מאשר בצורה שכתבתיו. לכל אופציה המעלות והחסרונות שלה
 def return_suspicions_types(log_list:list):
     suspicions_types_per_ip = {}
     for line in log_list:
@@ -47,3 +51,8 @@ def return_suspicions_types(log_list:list):
 def filtering_suspicions(suspicions_dict:dict):
     filtered_suspicions = {item[0]:item[1] for item in suspicions_dict.items() if len(item[1]) > 2}
     return filtered_suspicions
+
+hours = list(map(lambda line: int(line[0][11:13]), log_list))
+size_as_kb = list(map(lambda line: int(line[5]) / 1024, log_list))
+sensitive_port_lines = list(filter(lambda line: True if line[3] == "22" else True if line[3] == "23" else True if line[3] == "3389" else False, log_list))
+night_activity_lines = list(filter(lambda line: True if  0 < int(line[0][11:13]) < 6 else False, log_list))
